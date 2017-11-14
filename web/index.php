60f1a603c73141bb61bb40906879aa513506d277
Antoine07 <antoine.lucsko@wanadoo.fr>
2017-11-14 17:02:30 +0100

    architecture app

--- web/index.php
+++ web/index.php
@@ -1,96 +1,16 @@
 <?php
+/**
+ *  Point d'entrée de l'application
+ * 
+ */
 
-session_start();
-
-define('SALT', 'pU1TIYoa6f3Gmqkg0UviAewPvkCLc9mCxKJsVFUX2cU9CiasvsLei');
-
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$method = $_SERVER['REQUEST_METHOD'];
-
-// PDO
-$defaults = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // PDO remonte les erreurs SQL, sinon il retourne une bête erreur PHP
-  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // retournera les données dans un tableau associatifs
-];
-
-$pdo = new PDO('mysql:host=localhost;dbname=sheep', 'root', '', $defaults);
+// bootstrap de l'application 
+require_once __DIR__.'/../app.php';
 
 if ('/' === $uri) {
-
-	include __DIR__ . '/../views/front/auth.php';
-	
+	index();
 } elseif ('/auth' == $uri and $method == 'POST') {
-
-	$flagToken = false;
-	$token = $_POST['token'];
-	if ( !empty($token) ) {
-		foreach (range(0, 5) as $v) {
-			if ( ($token == md5(date('Y-m-d h:i:00', time() - $v * 60) . SALT))) {
-				$flagToken = true;
-			}
-		}
-	}
-
-	if( $flagToken == false ){
-		header('Location: /');
-		exit;
-	}
-
-	// API php pour filtre les données
-	$rules = [
-		'email'        => FILTER_VALIDATE_EMAIL,
-		'password'      => [
-			'filter'  => FILTER_CALLBACK,
-			'options' => function ($pass) {
-				if ( strlen($pass) < 4 ) {
-					return false;
-				} else {
-					return $pass ;
-				}
-			},
-		]
-	];
-
-	$sanitize = filter_input_array(INPUT_POST, $rules);
-
-    $email = $sanitize['email'];
-    $password =  $sanitize['password'];
-
-	$prepare = $pdo->prepare("
-		SELECT id, password FROM users WHERE email=? 
-	");
-
-	$prepare->bindValue(1, $email);
-	$prepare->execute();
-
-	$stmt = $prepare->fetch();
-
-	$_SESSION['email'] = null;
-	$_SESSION['email'] = $sanitize['email'];
-
-    if ( $stmt  == false) {
-		// redirection avec message d'erreur
-		$_SESSION['message'] = "Une erreur dans le mot de passe ou email";
-
-		header('Location: /');
-		exit;
-
-    } else {
-		if( password_verify($sanitize['password'], $stmt['password']) )
-		{
-			session_regenerate_id(true); // crée un nouvel identifiant
-			$_SESSION['auth'] = $stmt['id'];
-
-			header('Location: admin');// redirection vers une page sécurisée
-			exit;
-		}else{
-			$_SESSION['message'] = "Erreur dans le mot de passe ou email";
-			
-			header('Location: /');
-			exit;
-		}
-	}
-
+	auth();
 } elseif( $uri == '/admin' ){
 	if( !isset($_SESSION['auth']) ){
 		$_SESSION['message'] = "Vous n'avez pas l'autorisation";
