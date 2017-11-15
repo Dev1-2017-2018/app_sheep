c28e4573c1e651f586ba7eb1dc91da48ad1394a4
Antoine07 <antoine.lucsko@wanadoo.fr>
2017-11-15 11:05:28 +0100

    dashboard

--- views/back/dashboard.php
+++ views/back/dashboard.php
@@ -1,11 +1,11 @@
 <?php ob_start() ; ?>
-<section class="main dashboard">
-    <section class="graph grid-2"> 
+<section class="sheep__main dashboard">
+    <section class="sheep__graph grid-2"> 
         <div>un div ou n'importe quoi d'autre</div>
         <div>un div ou n'importe quoi d'autre</div>
     </section>
 
-    <section class="spending grid-1"> 
+    <section class="sheep__spending grid-1"> 
         <div>un div ou n'importe quoi d'autre</div>
     </section>
 
